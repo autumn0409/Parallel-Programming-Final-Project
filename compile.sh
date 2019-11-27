50ff53701dda7de7f3dbb7f9583eaf49b170c747
autumn0409 <autumn0409.cs08g@nctu.edu.tw>
2019-11-27 21:13:57 +0800

    modify compile script of parallel version

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@ then
     g++ ./code/serial/serial_router.cpp ./code/util/parser.cpp ./code/util/timer.cpp -o ./bin/serial_router
 elif [ $1 = "parallel" ]
 then 
-    g++ -fopenmp ./code/parallel/router.cpp ./code/util/parser.cpp ./code/util/timer.cpp -o ./bin/router
+    g++ ./code/parallel/router.cpp ./code/util/parser.cpp ./code/util/timer.cpp -o ./bin/router
 fi
