bc97c7d926af55098ea956b2c481365eb6b636ae
autumn0409 <autumn0409.cs08g@nctu.edu.tw>
2019-12-19 21:22:35 +0800

    Change ./run.sh output format

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-TIMEFORMAT=$'%3lR'
+TIMEFORMAT=$'  real\t%3lR\n  user\t%3lU\n  sys\t%3lS'
 
 echo -e "Serial:"
-echo -n "Execution time: "
+echo -e "Time statistics: "
 time ./bin/serial_router ./testing\ data/gr$1x$1.in ./output\ data/$1.out
 ./verify\ program/verify_linux ./testing\ data/gr$1x$1.in ./output\ data/$1.out
 
 for ((i=2; i<=$2; i++))
 do
     echo -e "\nThread count = $i:"
-    echo -n "Execution time: "
+    echo -e "Time statistics: "
     time ./bin/router ./testing\ data/gr$1x$1.in ./output\ data/$1.out $i
     ./verify\ program/verify_linux ./testing\ data/gr$1x$1.in ./output\ data/$1.out
 done
